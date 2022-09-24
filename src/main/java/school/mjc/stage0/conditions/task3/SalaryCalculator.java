05dfc0b1f728c983fcad6336c7fa4e1c1c061349
abrormukimov <abrormm34@gmail.com>
2022-09-24 23:04:09 +0500

    Update tasks

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary <= 10000) {
+            salary *= 85 / 100;
+            System.out.println(salary);
+        } else if (salary > 10000 && salary <= 20000) {
+            salary *= 82 / 100;
+            System.out.println(salary);
+        } else if (salary > 20000) {
+            salary *= 80 / 100;
+            System.out.println(salary);
+        } else if (salary <= 0) {
+            System.out.println("wrong input!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        if (month == 3) {
+            System.out.println("Spring");
+        } else if (month == 6) {
+            System.out.println("Summer");
+        } else if (month == 9) {
+            System.out.println("Fall");
+        } else if (month == 12) {
+            System.out.println("Winter");
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
